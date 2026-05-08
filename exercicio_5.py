1faf6ad21eb06f13321c6fa89302b3c7da77997b
annaaaguiar <annaaguiar.galo@gmail.com>
2026-05-08 10:18:40 -0300

    Calculate highest average from organism list

--- exercicio_5.py
+++ exercicio_5.py
@@ -40,3 +40,19 @@ O cálculo de média já foi feito em sala e pode ser usado de exemplo.
 lista_de_organismos = [[50, 50, 50], [125, 99, 12], [19, 91, 42], [40, 189, 0], [1, 0, 0], [100, 100, 70], [99, 12, 12]]
 
 # Fazer a partir daqui
+
+lista_de_organismos = [[100, 200, 300], [1, 99, 10000], [0, 0, 1]]
+
+maior_media = 0
+indice_maior = 0
+
+for i in range(len(lista_de_organismos)):
+    organismo = lista_de_organismos[i]
+    media = sum(organismo) / len(organismo)
+    
+    if media > maior_media:
+        maior_media = media
+        indice_maior = i
+        
+print(f"Maior média: {maior_media}")
+print(f"Índice do organismo: {indice_maior}")
